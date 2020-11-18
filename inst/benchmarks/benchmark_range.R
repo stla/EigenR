1f9a572c4096043570d52bdc8e71156d38abe0f2
stla <laurent_step@yahoo.fr>
2020-11-18 16:28:41 +0100

    DESCRIPTION

--- inst/benchmarks/benchmark_range.R
+++ inst/benchmarks/benchmark_range.R
@@ -2,15 +2,15 @@ library(EigenR)
 library(microbenchmark)
 
 set.seed(666)
-nrows <- 8
-ncols <- 4
+nrows <- 900
+ncols <- 600
 M <- round(matrix(rgamma(nrows*ncols, 10, 1), nrow = nrows, ncol = ncols), digits = 1)
 M <- cbind(M, M[,1]+M[,2], M[,2]+2*M[,3])
 
 microbenchmark(
   QR = Eigen_range(M, "QR"),
   LU = Eigen_range(M, "LU"),
-  LU = Eigen_range(M, "COD"),
+  COD = Eigen_range(M, "COD"),
   times = 20
 )
 
